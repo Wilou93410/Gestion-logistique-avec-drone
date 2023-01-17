e90946aed17afd72db21f3fca08644a52ad284cd
zarcroft <120554160+zarcroft@users.noreply.github.com>
2023-01-17 13:57:17 +0100

    Update main.php

--- pageconnexion/main.php
+++ pageconnexion/main.php
@@ -1,27 +1,29 @@
-
-
 <?php
-require "conf.php"
+
 if(isset($_POST['submit'])) {
     // récupération des informations d'identification
     $username = $_POST['username'];
     $password = $_POST['password'];
 
     // connexion à la base de données
-    $conn = new mysqli("host", "username", "password", "dbname");
+    $conn = new mysqli("localhost", "admin", "admin", "utilisateur");
 
     // vérification des informations d'identification
-    $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+    $query = "SELECT * FROM utilisateur WHERE username = '$username' AND password = '$password'";
     $result = $conn->query($query);
 
     if ($result->num_rows > 0) {
+        
         // informations d'identification valides
+        // récupération de la valeur de "userrole" dans la base de données
+        $row = $result->fetch_assoc();
+        $user_role = $row['userrole'];
         // démarrage de la session et stockage des informations d'utilisateur
         session_start();
         $_SESSION['username'] = $username;
-        $_SESSION['user_role'] = $user_role;
+        $_SESSION['userrole'] = $user_role;
         // redirection vers la page protégée
-        header("Location: $_user_role.php");
+        header("Location: $user_role.php");
     } else {
         // informations d'identification incorrectes
         echo "Nom d'utilisateur ou mot de passe incorrect";
@@ -39,4 +41,3 @@ if(isset($_POST['submit'])) {
  
 </html>
 <!--cette page permet d'effectuer la connexion des utilisateurs et de vérifié leurs identifiants, en fonctions de leurs rôles ca les renvoies sur une page soit admin soit employés. -->
-
