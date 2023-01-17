f49d2bc5dc0553fd15fb02f1c1e83360ac2f5e8b
zarcroft <120554160+zarcroft@users.noreply.github.com>
2023-01-17 11:52:15 +0100

    Update admin.php

--- pageconnexion/admin.php
+++ pageconnexion/admin.php
@@ -1,8 +1,13 @@
+<!DOCTYPE html>
 <html>
-    <h1>Page Admin</h1>
-    <p>faite attention, sur cette page vous avez tout les droits, ne supprimez pas ou ne modifiez pas quelque choses sans avoir ce que vous faite au préalable, merci de votre compréhension</p>
-    <input type button>modifier</input>
-    <input type button>supprimer</input>
-    <input type button>ajouter</input>
-
+    <head>
+        <title>admin</title>
+    </head>
+    <body>
+        <h1>Page Admin</h1>
+        <p>faite attention, sur cette page vous avez tout les droits, ne supprimez pas ou ne modifiez pas quelque choses sans avoir ce que vous faite au préalable,vous serez tenus pour responsable en cas de fausse manipulation, merci de votre compréhension</p>
+        <button type="button" onclick="window.location.href='modifadmin.php';">modifier</button>
+        <button type="button" onclick="window.location.href='suppradmin.php';">supprimer</button>
+        <button type="button" onclick="window.location.href='ajoutadmin.php';">ajouter</button>
+    </body>
 </html>
