648e199a5e26c303ca8502e17ef80e15ac22f39c
zarcroft <120554160+zarcroft@users.noreply.github.com>
2023-01-17 10:39:22 +0100

    Create admin.php

---
+++ pageconnexion/admin.php
@@ -0,0 +1,8 @@
+<html>
+    <h1>Page Admin</h1>
+    <p>faite attention, sur cette page vous avez tout les droits, ne supprimez pas ou ne modifiez pas quelque choses sans avoir ce que vous faite au préalable, merci de votre compréhension</p>
+    <input type button>modifier</input>
+    <input type button>supprimer</input>
+    <input type button>ajouter</input>
+
+</html>
