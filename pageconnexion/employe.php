18c2733ee3b1facccf22461caa974e020d0788fa
zarcroft <120554160+zarcroft@users.noreply.github.com>
2023-01-17 14:02:13 +0100

    Add files via upload

---
+++ pageconnexion/employe.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Employe</title>
+    </head>
+    <body>
+        <h1>Carton</h1>
+        <p>Retrouve ici les informations des cartons</p>
+        <button type="button" onclick="window.location.href='modifadmin.php';">modifier</button>
+        <button type="button" onclick="window.location.href='suppradmin.php';">supprimer</button>
+        <button type="button" onclick="window.location.href='ajoutadmin.php';">ajouter</button>
+    </body>
+</html>
