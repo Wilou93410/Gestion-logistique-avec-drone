f8e2b57149f89967f842ee4cb82bf47e850b8663
zarcroft <120554160+zarcroft@users.noreply.github.com>
2023-01-31 09:57:04 +0100

    Add files via upload

--- pageconnexion/1.php
+++ pageconnexion/1.php
@@ -6,9 +6,10 @@
     <body>
         <h1>Page Admin</h1>
         <p>faite attention, sur cette page vous avez tout les droits, ne supprimez pas ou ne modifiez pas quelque choses sans avoir ce que vous faite au préalable,vous serez tenus pour responsable en cas de fausse manipulation, merci de votre compréhension</p>
-        <button type="button" onclick="window.location.href='modifadmin.php';">modifier</button>
-        <button type="button" onclick="window.location.href='suppradmin.php';">supprimer</button>
-        <button type="button" onclick="window.location.href='ajoutadmin.php';">ajouter</button>
-        <button type="button" onclick="window.location.href='utilisateur/createuser.php';">crée un utilisateur</button>
+        <button type="button" onclick="window.location.href='admin/modifadmin.php';">modifier</button>
+        <button type="button" onclick="window.location.href='admin/suppradmin.php';">supprimer</button>
+        <button type="button" onclick="window.location.href='admin/ajoutadmin.php';">ajouter</button>
+        <button type="button" onclick="window.location.href='admin/createuser.php';">crée un utilisateur</button>
+        <button type="button" onclick="window.location.href='utilisateur/afficherutilisateur.php';">Voir utilisateur</button>
     </body>
 </html>
