c43c6bcbd0e26a8ed491e3758e25e27995876c3f
zarcroft <120554160+zarcroft@users.noreply.github.com>
2023-02-06 10:17:04 +0100

    Add files via upload

--- pageconnexion/2.php
+++ pageconnexion/2.php
@@ -1,15 +1,28 @@
 <!DOCTYPE html>
 <html>
-<link rel="stylesheet" href="CSS/style.css">
+<link rel="stylesheet" href="CSS/style.scss">
     <head>
         <title>Employe</title>
     </head>
     <body>
         <h1>Carton</h1>
         <p>Retrouve ici les informations des cartons</p>
-        <button type="button" onclick="window.location.href='modifadmin.php';">modifier</button>
-        <button type="button" onclick="window.location.href='suppradmin.php';">supprimer</button>
-        <button type="button" onclick="window.location.href='ajoutadmin.php';">ajouter</button>
-        <button type="button" onclick="window.location.href='utilisateur/afficherutilisateur.php';">Voir utilisateur</button>
-    </body>
+
+               <div class ="btnn">  
+        <button type="button" onclick="window.location.href='';">
+        <span class="transition"></span>
+        <span class="gradient"></span>
+        <span class="label">ajouter cartons</span>
+        </button>
+
+         
+        <button type="button" onclick="window.location.href='';">
+        <span class="transition"></span>
+        <span class="gradient"></span>
+        <span class="label">informations cartons</span>
+        </button>
+
+</div>
+
+     </body>
 </html>
