06f5a190655471b0cfa88153ccb81a043030b1c8
zarcroft <120554160+zarcroft@users.noreply.github.com>
2023-02-14 08:48:50 +0100

    Update 2.php
    Ajout barre de recherche cartons

--- pageconnexion/2.php
+++ pageconnexion/2.php
@@ -26,3 +26,32 @@
 
      </body>
 </html>
+<?php
+require 'carton.php';
+
+if (isset($_POST['recherche'])) {
+    $recherche = $_POST['recherche'];
+    
+    // Préparation de la requête SQL avec des jointures pour inclure les informations de la table de destination et de la table de provenance
+    $sql = "SELECT cartons.*, destination.destination, fournisseur.nom, fournisseur.prenom, fournisseur.adresse, provenance.provenance FROM cartons
+        JOIN destination ON cartons.id_destination = destination.id_destination
+        JOIN fournisseur ON cartons.id_fournisseur = fournisseur.id_fournisseur
+        JOIN provenance ON cartons.id_provenance = provenance.id_provenance
+        WHERE cartons.contenu LIKE '%$recherche%' OR cartons.id_carton LIKE '%$recherche%' OR cartons.taille LIKE '%$recherche%' OR cartons.poids LIKE '%$recherche%' OR cartons.quantité LIKE '%$recherche%'";
+
+    // Exécution de la requête et stockage des résultats
+    $result = mysqli_query($dbh, $sql);
+    
+    // Vérifier si des résultats ont été trouvés
+    if (mysqli_num_rows($result) > 0) {
+    // Boucle à travers les enregistrements et affichage des informations du carton
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "CARTONS : " . " Provenance : " . $row["provenance"]. " Destination : " . $row["destination"]. " Quantité : " . $row["quantité"]. " Poids : " . $row["poids"]. " Taille : " . $row["taille"]. " Contenu : " . $row["contenu"]. "<br>";
+    }
+} else {
+    echo "Aucun résultat trouvé.";
+}}
+
+// Fermer la connexion à la base de données
+mysqli_close($dbh);
+?>
