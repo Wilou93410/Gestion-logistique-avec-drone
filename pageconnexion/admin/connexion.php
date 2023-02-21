98c505790ddbdbea35a542cfa834712c2aaa8c74
WillBero <122380880+WillBero@users.noreply.github.com>
2023-02-21 11:04:27 +0100

    Delete pageconnexion/admin directory

--- pageconnexion/admin/connexion.php
+++
@@ -1,9 +0,0 @@
-<?php 
-try {
-                $dbh =new PDO('mysql:host=localhost;dbname=cartons','admin' , 'admin');
-                $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        }
-        catch(PDOException $e){
-                echo $e->getMessage();}
-
-?>
