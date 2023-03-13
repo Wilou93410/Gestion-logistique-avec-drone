655e02bc01ed0a661e59294aff1ede682581a872
zarcroft <120554160+zarcroft@users.noreply.github.com>
2023-03-13 09:26:37 +0100

    Update createuser.php

--- PAGE WEB/connexion/admin/cruduser/createuser.php
+++ PAGE WEB/connexion/admin/cruduser/createuser.php
@@ -1,30 +1,33 @@
-
 <?php 
 // Connexion à la base de données
-
-$dbh = new mysqli("localhost", "admin", "admin", "userscan");
+$dbh = new PDO('mysql:host=localhost;dbname=userscan', 'admin', 'admin');
+$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 // Récupération des données de la table "fonction"
-
-$droits = $dbh->query('SELECT * FROM droit');
+$permissions = $dbh->query('SELECT * FROM permission');
 
 // Stockage des données du formulaire
-
-if (!empty($_POST['nom']) && !empty($_POST['prenom']) && !empty($_POST['pseudo']) && !empty($_POST['password']) && !empty($_POST['droit'])) {
-    $name = $_POST['nom'];
-    $surname = $_POST['prenom'];
+if (!empty($_POST['name']) && !empty($_POST['firstname']) && !empty($_POST['pseudo']) && !empty($_POST['password']) && !empty($_POST['permission'])) {
+    $name = $_POST['name'];
+    $surname = $_POST['firstname'];
     $pseudo = $_POST['pseudo'];
     $password = $_POST['password'];
-    $droit = $_POST['droit'];
+    $permission = $_POST['permission'];
     
     // Insertion des données dans la table "utilisateur"
-
-    $sql = "INSERT INTO utilisateurs (nom, prenom, pseudo, password, droit) VALUES ( '$name', '$surname', '$pseudo', '$password', '$droit')";
+    $stmt = $dbh->prepare("INSERT INTO users (name, firstname, pseudo, password, permission) VALUES (:name, :firstname, :pseudo, :password, :permission)");
+    $stmt->bindParam(':name', $name);
+    $stmt->bindParam(':firstname', $surname);
+    $stmt->bindParam(':pseudo', $pseudo);
+    $stmt->bindParam(':password', $password);
+    $stmt->bindParam(':permission', $permission);
     
-    if ($dbh->query($sql) === TRUE) {
+    if ($stmt->execute()) {
         echo "Données utilisateur insérées avec succès";
     } else {
-        echo "Erreur";}}
+        echo "Erreur";
+    }
+}
 ?>
 <!-- formulaire de création d'utilisateur -->
 <!doctype html>
@@ -38,11 +41,11 @@ if (!empty($_POST['nom']) && !empty($_POST['prenom']) && !empty($_POST['pseudo']
   <body>
 <H1>création d'utilisateur</H1>
   <form method="post">
-  <label for="nom">nom :</label>
-  <input type="text" id="nom" name="nom" required>
+  <label for="name">name :</label>
+  <input type="text" id="name" name="name" required>
   <br><br>
-  <label for="prenom">prenom :</label>
-  <input type="text" id="prenom" name="prenom" required>
+  <label for="firstname">firstname :</label>
+  <input type="text" id="firstname" name="firstname" required>
   <br><br>
   <label for="pseudo">pseudo :</label>
   <input type="text" id="pseudo" name="pseudo" required>
@@ -51,10 +54,9 @@ if (!empty($_POST['nom']) && !empty($_POST['prenom']) && !empty($_POST['pseudo']
   <input type="password" id="password" name="password" required>
   <br><br>
 
-<P> 1 = Administrateur; 2 = Droit employés </p>
-<select class="form-select" aria-label="Default select example" name="droit" required>
-<?php foreach ($droits as $droit): ?>
-    <option value="<?= $droit['droit']?>">fonction : <?= $droit['droit']?></option>
+<select class="form-select" aria-label="Default select example" name="permission" required>
+<?php foreach ($permissions as $permission): ?>
+    <option value="<?= $permission['permission']?>">fonction : <?= $permission['permission']?></option>
     <?php endforeach; ?>  
 </select>
 <br><br>
@@ -66,5 +68,5 @@ if (!empty($_POST['nom']) && !empty($_POST['prenom']) && !empty($_POST['pseudo']
     
 </footer>
 <br><br>
-<button onclick="window.location.href = '/pageconnexion/1.php';">Retour</button>
+<button onclick="window.location.href = '/admin/admin.php';">Retour</button>
 </html>
