6e19fa44705a66f0b20320fda89e6c454ab42108
zarcroft <120554160+zarcroft@users.noreply.github.com>
2023-03-13 09:42:06 +0100

    Update deleteuser.php

--- PAGE WEB/connexion/admin/cruduser/deleteuser.php
+++ PAGE WEB/connexion/admin/cruduser/deleteuser.php
@@ -1,44 +1,47 @@
 <?php
-// Connexion à la base de données
-require 'conf.php';
+$dbh = new PDO('mysql:host=localhost;dbname=userscan', 'admin', 'admin');
 
-// Récupération des données de la table "utilisateur"
-$sql = "SELECT * FROM utilisateur";
-$result = mysqli_query($dbh, $sql);
-$utilisateurs = mysqli_fetch_all($result, MYSQLI_ASSOC);
+// Récupération des données de la table "users"
+$sql = "SELECT * FROM users";
+$result = $dbh->query($sql);
+$users = $result->fetchAll(PDO::FETCH_ASSOC);
 
-// Vérification de la soumission du formulaire
-if (isset($_POST['utilisateur'])) {
-    $username = $_POST['utilisateur'];
-    // Prepare and execute the SQL statement to delete the user
-    $sql = "DELETE FROM utilisateur WHERE username = '$username'";
-    mysqli_query($dbh, $sql);
+if (isset($_POST['user'])) {
+    $pseudo = $_POST['user'];
 
-    if ($dbh->query($sql) === TRUE) {
-       echo "Données utilisateur supprimé avec succès";
-  } else {
-      echo "Erreur";}}
+    $sql = "DELETE FROM users WHERE pseudo = ?";
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute([$pseudo]);
 
+    if ($stmt->rowCount() > 0) {
+        echo "Données utilisateur supprimées avec succès";
+    } else {
+        echo "Erreur";
+    }
+}
 ?>
+
 <!doctype html>
 <html lang="en">
 <link rel="stylesheet" href="style.css">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Suppression d'utilisateur</title>
-  </head>
-  <body>
-<h1>Suppression d'utilisateur</h1>
-  <form method="post">
-    <select class="form-select" aria-label="Default select example" name="utilisateur" required>
-    <?php foreach ($utilisateurs as $utilisateur): ?>
-        <option value="<?= $utilisateur['username']?>"><?= $utilisateur['username']?></option>
-    <?php endforeach; ?>
-    </select>
-    <input type="submit" value="supprimer">
-  </form>
-  <footer>
-  </footer>
-  <button onclick="window.location.href = '/pageconnexion/1.php';">Retour</button>
+</head>
+
+<body>
+    <h1>Suppression d'utilisateur</h1>
+    <form method="post">
+        <select class="form-select" aria-label="Default select example" name="user" required>
+            <?php foreach ($users as $user): ?>
+                <option value="<?= $user['pseudo']?>"><?= $user['pseudo']?></option>
+            <?php endforeach; ?>
+        </select>
+        <input type="submit" value="supprimer">
+    </form>
+    <footer>
+    </footer>
+    <button onclick="window.location.href = '/pageconnexion/1.php';">Retour</button>
 </html>
