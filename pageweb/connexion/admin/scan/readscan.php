06233b7de2e830aac088efe6a3583170b3319291
zarcroft <120554160+zarcroft@users.noreply.github.com>
2023-03-31 10:21:36 +0200

    Update readscan.php

--- pageweb/connexion/admin/scan/readscan.php
+++ pageweb/connexion/admin/scan/readscan.php
@@ -1,65 +1,68 @@
 <?php
-session_start(); 
+session_start();
 if ($_SESSION['permission'] !== "admin") {
-    header("Location: ../../index.php"); 
+    header("Location: ../../index.php");
+    exit;
 }
 
 require "../../../config/configadmin.php";
 
-$sql = "SELECT * FROM scan INNER JOIN users on scan.id_user = users.id_user";
 $search = '';
+$sql = "SELECT * FROM scan INNER JOIN users ON scan.id_user = users.id_user";
+$params = array();
+
 if(isset($_POST['search'])) {
     $search = $_POST['search'];
-    $sql .= " WHERE name LIKE '%$search%' OR firstname LIKE '%$search%' OR pseudo LIKE '%$search%' OR dates LIKE '%$search%' OR id_carton LIKE '%$search%'";
+    $sql .= " WHERE name LIKE :search OR firstname LIKE :search OR pseudo LIKE :search OR dates LIKE :search OR id_carton LIKE :search";
+    $params['search'] = '%' . $search . '%';
 }
-$result = $dbh->query($sql);
-$scans = $result->fetchAll(PDO::FETCH_ASSOC);
+
+$stmt = $dbh->prepare($sql);
+$stmt->execute($params);
+$scans = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <!doctype html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <LINK href="../../../style/style.css" rel="stylesheet" type="text/css">
     <title>Affichage scans</title>
-  </head>
-  <body>
-    <h1>Utilisateurs</h1>
+</head>
+<body>
+<h1>Utilisateurs</h1>
 
-    <form method="post" class=recherche>
-        <label for="search">Rechercher :</label>
-        <input type="text" name="search" value="<?= $search ?>">
-        <button type="submit">Rechercher</button>
-    </form>
+<form method="post" class=recherche>
+    <label for="search">Rechercher :</label>
+    <input type="text" name="search" value="<?= htmlspecialchars($search) ?>">
+    <button type="submit">Rechercher</button>
+</form>
 <br>
-    <table>
-
-      <tr>
+<table>
+    <tr>
         <th>id_scan</th>
         <th>carton</th>
         <th>date</th>
         <th>pseudo</th>
         <th>nom</th>
         <th>prénom</th>
-      </tr>
+    </tr>
 
-      <?php foreach ($scans as $scan): ?>
-        
+    <?php foreach ($scans as $scan): ?>
         <tr>
-          <td><?= $scan['id_scan'] ?></td>
-          <td><?= $scan['id_carton'] ?></td>
-          <td><?= $scan['dates'] ?></td>
-          <td><?= $scan['pseudo'] ?></td>
-          <td><?= $scan['name'] ?></td>
-          <td><?= $scan['firstname'] ?></td>
+            <td><?= $scan['id_scan'] ?></td>
+            <td><?= $scan['id_carton'] ?></td>
+            <td><?= $scan['dates'] ?></td>
+            <td><?= $scan['pseudo'] ?></td>
+            <td><?= $scan['name'] ?></td>
+            <td><?= $scan['firstname'] ?></td>
         </tr>
+    <?php endforeach; ?>
 
-      <?php endforeach; ?>
-
-    </table>
-    <div class="deco">
+</table>
+<div class="deco">
     <button onclick="window.location.href = '../admin.php';">retour</button>
-
-  </body>
+</div>
+</body>
 </html>
