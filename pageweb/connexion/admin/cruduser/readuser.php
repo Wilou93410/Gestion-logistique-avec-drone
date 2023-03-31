4cef5f1472dc95587d647923695518feb70d3cc6
zarcroft <120554160+zarcroft@users.noreply.github.com>
2023-03-31 10:44:47 +0200

    Update readuser.php

--- pageweb/connexion/admin/cruduser/readuser.php
+++ pageweb/connexion/admin/cruduser/readuser.php
@@ -6,11 +6,39 @@ if ($_SESSION['permission'] !== "admin") {
 
 require "../../../config/configadmin.php";
 
+$search = "";
+$order_by = "";
+
 if (isset($_GET['search'])) {
   $search = $_GET['search'];
+}
+
+if (isset($_GET['order_by'])) {
+  $order_by = $_GET['order_by'];
+}
+
+$order_by_options = array(
+  "pseudo" => "Pseudo",
+  "name" => "Nom",
+  "firstname" => "Prénom",
+  "permission" => "Rôle"
+);
+
+if ($order_by && !array_key_exists($order_by, $order_by_options)) {
+  $order_by = "";
+}
+
+if ($search && $order_by) {
+  $sql = "SELECT * FROM users WHERE pseudo LIKE :search OR name LIKE :search OR firstname LIKE :search OR permission LIKE :search ORDER BY $order_by";
+  $stmt = $dbh->prepare($sql);
+  $stmt->execute(['search' => "%$search%"]);
+} elseif ($search) {
   $sql = "SELECT * FROM users WHERE pseudo LIKE :search OR name LIKE :search OR firstname LIKE :search OR permission LIKE :search";
   $stmt = $dbh->prepare($sql);
   $stmt->execute(['search' => "%$search%"]);
+} elseif ($order_by) {
+  $sql = "SELECT * FROM users ORDER BY $order_by";
+  $stmt = $dbh->query($sql);
 } else {
   $sql = "SELECT * FROM users";
   $stmt = $dbh->query($sql);
@@ -34,9 +62,23 @@ $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     <form method="get" class=recherche>
 
-      <label for="search">Recherche :</label>
-      <input type="text" id="search" name="search" placeholder="rechercher un utilisateur">
+ 
+      <input type="text" id="search" name="search" placeholder="rechercher un utilisateur" value="<?= $search ?>">
       <button type="submit">Rechercher</button>
+
+      </br>
+      <div class ="box">
+
+      <select id="order_by" name="order_by">
+        <option value=""></option>
+        <?php foreach ($order_by_options as $option_key => $option_label): ?>
+          <option value="<?= $option_key ?>" <?= $option_key == $order_by ? "selected" : "" ?>><?= $option_label ?></option>
+        <?php endforeach; ?>
+      </select>
+
+        </div>
+
+      <button type="submit">Trier</button>
 
     </form>
     <br>
@@ -64,5 +106,4 @@ $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
       <button onclick="window.location.href = '../admin.php';">retour</button>
     </div>
 
-  </body>
-</html>
+  </body
